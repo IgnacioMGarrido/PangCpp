aa1d35d8a613fbf6ee22f7c627e25e76eaa1e98a
Ignacio Martinez <nama1510@gmail.com>
2020-01-16 16:34:28 +0100

    Change order in life vector

--- swalib_example/Code/Game/Entities/EntityTypes/HUD_entity.cpp
+++ swalib_example/Code/Game/Entities/EntityTypes/HUD_entity.cpp
@@ -12,20 +12,22 @@ cEHud::cEHud(const char* cHeartSpritePath, const char* cGameOverSpritePath, cons
     assert(pRenderComp_1 != nullptr);
     AddComponent<cRenderComp&>(*pRenderComp_1);    // Insert render component.
     pRenderComp_1->SetSpritePosition(vmake((SCR_WIDTH / 2) - 500, SCR_HEIGHT - 50));
-    m_vHeartRenderComp.push_back(pRenderComp_1);
 
     cRenderComp* pRenderComp_2 = new cRenderComp(cHeartSpritePath, vmake(fRadius * 6.0f, fRadius * 6.0f));
     assert(pRenderComp_2!= nullptr);
     AddComponent<cRenderComp&>(*pRenderComp_2);    // Insert render component.
     pRenderComp_2->SetSpritePosition(vadd(pRenderComp_1->GetSpritePosition(), vmake(100 , 0)));
-    m_vHeartRenderComp.push_back(pRenderComp_2);
 
 
     cRenderComp* pRenderComp_3 = new cRenderComp(cHeartSpritePath, vmake(fRadius * 6.0f, fRadius * 6.0f));
     assert(pRenderComp_3 != nullptr);
     AddComponent<cRenderComp&>(*pRenderComp_3);
     pRenderComp_3->SetSpritePosition(vadd(pRenderComp_1->GetSpritePosition(), vmake(200, 0)));
+
     m_vHeartRenderComp.push_back(pRenderComp_3);
+    m_vHeartRenderComp.push_back(pRenderComp_2);
+    m_vHeartRenderComp.push_back(pRenderComp_1);
+
 
     m_pWinRenderComp = new cRenderComp(cWinSpritePath, vmake(fRadius * 40.0f, fRadius * 40.0f));
     assert(m_pWinRenderComp != nullptr);
